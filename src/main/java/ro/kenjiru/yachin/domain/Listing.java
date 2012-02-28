376f004ed5d94f6d1a2c0a980ce18c805c06bb44
kenjiru <kenjiru.ro@gmail.com>
2012-02-28 23:49:02 +0100

    Added a bean for the Listing entity.

---
+++ src/main/java/ro/kenjiru/yachin/domain/Listing.java
@@ -0,0 +1,44 @@
+package ro.kenjiru.yachin.domain;
+
+import java.util.Date;
+
+public class Listing {
+	private Long id;
+	private String title;
+	private Date submitDate;
+	private Integer noRooms;
+
+	public Listing() {}
+
+	public Long getId() {
+		return id;
+	}
+
+	private void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public Date getSubmitDate() {
+		return submitDate;
+	}
+
+	public void setSubmitDate(Date submitDate) {
+		this.submitDate = submitDate;
+	}
+
+	public Integer getNoRooms() {
+		return noRooms;
+	}
+
+	public void setNoRooms(Integer noRooms) {
+		this.noRooms = noRooms;
+	}
+}
